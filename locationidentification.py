9115ffaaedab29df9d0dc1c0dcbca5ffcb0e9204
hippyhippohops <akravi923@gmail.com>
2019-06-14 16:16:07 +0800

    Update locationidentification.py

--- locationidentification.py
+++ locationidentification.py
@@ -23,7 +23,31 @@ def location_identification(binaryMatrix, robot_image_binary_matrix, k):
 		row += 1
 	return 0
 
+def find_image(big_image, small_image):
+	#This function takes in 2 arguments and returns the position of the small image inside the big image, if there is
+	#take note that this function was written for opencv, need to change certain parts 
+	#link: https://stackoverflow.com/questions/29663764/determine-if-an-image-exists-within-a-larger-image-and-if-so-find-it-using-py
+	big_image = np.atleast_3d(big_image)
+    	small_image = np.atleast_3d(small_image)
+    	H, W, D = im.shape[:3]
+    	h, w = tpl.shape[:2]
 
+    	# Integral image and template sum per channel
+    	sat = im.cumsum(1).cumsum(0)
+    	tplsum = np.array([tpl[:, :, i].sum() for i in range(D)])
+
+    	# Calculate lookup table for all the possible windows
+    	iA, iB, iC, iD = sat[:-h, :-w], sat[:-h, w:], sat[h:, :-w], sat[h:, w:] 
+    	lookup = iD - iB - iC + iA
+    	# Possible matches
+    	possible_match = np.where(np.logical_and.reduce([lookup[..., i] == tplsum[i] for i in range(D)]))
+
+    	# Find exact match
+    	for y, x in zip(*possible_match):
+        	if np.all(im[y+1:y+h+1, x+1:x+w+1] == tpl):
+            		return (y+1, x+1)
+
+    	raise Exception("Image not found")
 
 def rotation():
 	"""
